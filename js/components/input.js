2667e2612345869e15ad2100b9f3fae4a2d2e564
Olivier-Kango <olivierkango@gmail.com>
2024-01-02 23:58:05 +0200

    Add constructor onChangeHandler to tagSelectionHandler

--- js/components/input.js
+++ js/components/input.js
@@ -1,5 +1,10 @@
 class Input {
   constructor(props) {
-    
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+    this.onKeyDownHandler = this.onKeyDownHandler.bind(this);
+    this.onKeyUpHandler = this.onKeyUpHandler.bind(this);
+    this.openSelectMenuHandler = this.openSelectMenuHandler.bind(this);
+    this.closeSelectMenuHandler = this.closeSelectMenuHandler.bind(this);
+    this.tagSelectionHandler = this.tagSelectionHandler.bind(this);
   }
 }
