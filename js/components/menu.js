db489c187a76575d1e6d5d186697c2b38f13a673
Olivier-Kango <olivierkango@gmail.com>
2024-01-03 01:32:29 +0200

    Add allowedTags array

---
+++ js/components/menu.js
@@ -0,0 +1,14 @@
+// allowedTags array
+
+const allowedTags = [
+  {
+    id: 'page-title',
+    tag: 'h1',
+    label: 'Heading 1',
+  },
+  {
+    id: 'paragraph',
+    tag: 'p',
+    label: 'Paragraph',
+  },
+];
