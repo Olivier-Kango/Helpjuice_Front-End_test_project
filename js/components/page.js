f7330f6b2cf3b45b9c01fc846412bced7f76daa1
Olivier-Kango <olivierkango@gmail.com>
2024-01-03 02:17:53 +0200

    set initialInput

--- js/components/page.js
+++ js/components/page.js
@@ -1,3 +1,10 @@
 import Input from './input.js';
 import { setCaretToEnd } from '../functions/caretHelpers.js';
-import { random } from '../functions/random.js';
+import random from '../functions/random.js';
+
+const initialInput = {
+  id: random(),
+  html: '',
+  tag: 'p',
+  placeholder: 'Type / for blocks, @ to link docs or people',
+};
