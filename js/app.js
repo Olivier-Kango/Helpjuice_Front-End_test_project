b2777db4770c1cd3bdce1c20105456fc6c4ad0e9
Olivier-Kango <olivierkango@gmail.com>
2024-01-03 20:05:27 +0200

    Add header

--- js/app.js
+++ js/app.js
@@ -1,27 +1,12 @@
-import Input from './components/input.js';
-// import EditablePage from './components/page.js';
+import EditablePage from './components/page.js';
 
-// const editablePage = new EditablePage();
-// const appContainer = document.getElementById('app');
+document.addEventListener('DOMContentLoaded', () => {
+  const editablePage = new EditablePage();
+  const appContainer = document.getElementById('app');
 
-// appContainer.appendChild(editablePage.render());
-
-// Your testing code goes here
-const inputContainer = document.getElementById('inputContainer');
-
-// Create an instance of Input
-const input = new Input({
-  id: 'uniqueId', // Provide a unique ID
-  html: '', // Initial HTML content
-  tag: 'p', // Initial tag
-  placeholder: 'Enter text here Olk', // Placeholder text
-  addInput: () => {}, // Function to add input
-  deleteInput: () => {}, // Function to delete input
-  updatePage: (data) => {
-    // Logique pour mettre à jour la page avec les données fournies (data)
-    console.log('Update page with:', data);
-  }, // Function to update page
+  if (appContainer) {
+    appContainer.appendChild(editablePage.render());
+  } else {
+    console.error("Element with ID 'app' not found.");
+  }
 });
-
-// Render the Input component
-inputContainer.appendChild(input.render());
